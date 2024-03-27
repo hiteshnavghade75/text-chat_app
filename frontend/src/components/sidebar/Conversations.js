2f86b187d7b643c981c4053a53a25c3d1a3f2f99
hiteshnavghade75 <hiteshnavghade75@gmail.com>
2024-03-27 09:14:30 +0530

    .

--- frontend/src/components/sidebar/Conversations.js
+++ frontend/src/components/sidebar/Conversations.js
@@ -28,22 +28,3 @@ export default Conversations
 
 
 
-
-// STARTER CODE CONVERSATIONS
-// import React from 'react'
-// import Conversation from './Conversation'
-
-// const Conversations = () => {
-//   return (
-//     <div className='py-2 flex flex-col overflow-auto'>
-//         <Conversation/>
-//         <Conversation/>
-//         <Conversation/>
-//         <Conversation/>
-//         <Conversation/>
-//         <Conversation/>
-//     </div>
-//   )
-// }
-
-// export default Conversations
